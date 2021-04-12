e4739ae49b8c50883ac41670f493297e54cec17b
heheboye <ezequiel.4.m@gmail.com>
2021-04-12 14:29:17 -0300

    anecdotes

---
+++ src/App.js
@@ -0,0 +1,51 @@
+import React, { useState } from 'react'
+import Button from './Button'
+
+const App = () => {
+  const anecdotes = [
+    'If it hurts, do it more often',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
+  ]
+   
+  const [selected, setSelected] = useState(0)
+  const [votes, setVotes] = useState(new Array(anecdotes.length).fill(0))
+  
+  const nextQuote = () => setSelected(Math.floor(Math.random() * anecdotes.length))
+  
+  const upVote = () => {
+    let votesCopy = [...votes]
+    votesCopy[selected] += 1
+    setVotes(votesCopy)
+  }
+
+  const mostVotes = () => {
+    let max = 0
+    let indexMax = 0
+    votes.forEach((item, index) => {
+      if (item > max) {
+        max = item
+        indexMax = index
+      }
+    })
+    return indexMax
+  }
+
+  return (
+    <div>
+    <h1>Anecdote of the day</h1>
+      <p>{anecdotes[selected]}</p>
+      <p>has {votes[selected]} votes</p>
+      <Button handleClick={nextQuote} text='next anecdote' />
+      <Button handleClick={upVote} text='vote' />
+    <h1>Anecdote with most votes</h1>
+      <p>{anecdotes[mostVotes()]}</p>
+      <p>has {votes[mostVotes()]} votes</p>
+    </div>
+  )
+}
+
+export default App
---
+++ src/Display.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Display = ({value, type}) => {
+  return (
+    <p>
+      {type} {value}
+    </p>
+  ) 
+}
+
+export default Display
